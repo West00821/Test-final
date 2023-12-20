7a59a24d0075b9427a2f21e3378809b2cb8e4264
West00821 <West00821@yandex.ru>
2023-12-20 21:01:10 +0300

    Финальная версия

--- src/main/java/org/example/ReadXml.java
+++ src/main/java/org/example/ReadXml.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReadXml {
-    /*Поле из списка объектов, которые мы вытаскиваем из файла*/
+
     private static ArrayList<Object> objects = new ArrayList<>();
-   /* понять различие между == и equals */
+
 
     public static class XMLHandler extends DefaultHandler {
         @Override
@@ -27,7 +27,7 @@ public class ReadXml {
     }
 
     public static List<String> getAddress(String date, List<String> objectIds) {
-        // создание пустого списка
+
         List<String> address = new ArrayList<>();
         for (Object obj : objects) {
             String startDate = obj.getStartDate();
@@ -41,13 +41,7 @@ public class ReadXml {
         }
         return address;
     }
-    /*
-      В Java две даты можно сравнить, используя метод compareTo() интерфейса Comparable.
-      Этот метод возвращает '0', если обе даты равны, возвращает значение "больше 0",
-      если date1 идет после date2, и возвращает значение "меньше 0",
-      если date1 идет до date2.
-      + есть еще способ сравнения через equals а также через before и after
-     */
+
     private static boolean isDateInRange(String startDate, String endDate, String targetDate) {
         return startDate.compareTo(targetDate) <= 0 && endDate.compareTo(targetDate) >= 0;
     }
